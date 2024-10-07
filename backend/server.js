9fd855d045471c6856c44b229b2f4414bafc42de
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-10-07 13:17:14 -0700

    edit to both front and backend

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,27 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const TodoModel = require('./Models/Todo')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect(' ') // database connection
+mongoose.connect('mongodb+srv://mikeyvaldez92:Y0ung$ter@todo.7d6ue.mongodb.net/test') // database connection
 
-app.post('/add', (req, res => {
+app.get('/get', (req, res) => {
+    TodoModel.find()
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+app.post('/add', (req, res) => {
     const task = req.body.task;
-
-}))
+    TodoModel.create({
+        task: task
+    }).then(result => res.json(result))
+    .catch(err => res.json(err))
+})
 
 app.listen(3001, () => {
     console.log("Server is Runnig")
--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,21 +1,38 @@
-import React, { useState } from 'react'
+/* eslint-disable react/jsx-key */
+// eslint-disable-next-line no-unused-vars
+import React, { useEffect, useState } from 'react'
+import { BsCircleFill, BsFillTrashFill } from 'react-icons/bs'
 import Create from './Create'
+import axios from 'axios'
+
 
 function Home() {
     const [todos, setTodos] = useState([])
+    useEffect(() => {
+        axios.get('http://localhost:3001/get')
+        .then(result => setTodos(result.data))
+        .catch(err => console.log(err))
+    }, [])
   return (
     <div className='home'>
         <h2>Todo List</h2>
         <Create />
+        <br />
         {
             todos.length === 0
             ?
             <div><h2>No Record</h2></div>
-            :
+            :            
             todos.map(todo => (
-                <div>
-                    {todo}
+            <div className='task'>
+                <div className='checkbox'>
+                    <BsCircleFill className='icon'/>
+                    <p>{todo.task}</p>
                 </div>
+                <div>
+                    <span><BsFillTrashFill className='icon'/></span>
+                </div>
+            </div>
             ))
         }
     </div>
